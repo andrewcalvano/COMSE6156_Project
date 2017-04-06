0b80de42ddea04ac94d1b28def8e61d13dea5c58
Andrew Calvano <arc2205@columbia.edu>
2017-04-05 19:15:42 -0600

    Changed some default info in bbedgegrind tool.

--- valgrind/bbedgegrind/bbe_main.c
+++ valgrind/bbedgegrind/bbe_main.c
@@ -99,9 +99,9 @@ static void bbe_pre_clo_init(void)
 {
    VG_(details_name)            ("BBEdgegrind");
    VG_(details_version)         (NULL);
-   VG_(details_description)     ("the minimal Valgrind tool");
+   VG_(details_description)     ("Valgrind Tool To Monitor Basic Block Edge Coverage");
    VG_(details_copyright_author)(
-      "Copyright (C) 2002-2015, and GNU GPL'd, by Nicholas Nethercote.");
+      "Andrew Calvano");
    VG_(details_bug_reports_to)  (VG_BUGS_TO);
 
    VG_(details_avg_translation_sizeB) ( 275 );
