c6b8dae8c04cc7fe05ffc35e08166e179468d6d2
Andrew Calvano <arc2205@columbia.edu>
2017-04-15 13:40:03 -0600

    Mostly done with dynamorio client example.

--- dynamorio/bbedge_coverage.c
+++ dynamorio/bbedge_coverage.c
@@ -1,4 +1,6 @@
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 #include "dr_api.h"
 
 struct ThreadBBInfo {
@@ -31,10 +33,71 @@ struct BasicBlockRecordHead *global_bbs = NULL;
 
 FILE * mmap_fh = NULL;
 
+app_pc instrument_start_address = 0;
+app_pc instrument_end_address = 0;
+
+const char *instrument_module_name = NULL;
+
+const char *output_prefix = NULL;
+
+static void
+parse_arguments(int argc, const char *argv[])
+{
+    const char *arg = NULL;
+    /*
+        Valid arguments are 
+            -start_address 0xaddress
+            -end_address 0xaddress
+            -module_name module_name
+            -prefix output_prefix
+    */
+    for (int i = 1; i < argc; i++)
+    {
+        arg = argv[i];
+        if(!strcmp(arg, "-start_address"))
+        {
+            instrument_start_address = (app_pc) strtoul(argv[i+1], NULL, 16);
+            dr_fprintf(STDERR, "Only instrumenting addresses after 0x%08x\n", instrument_start_address);
+            i++;
+        }
+        else if (!strcmp(arg, "-end_address"))
+        {
+            instrument_end_address = (app_pc) strtoul(argv[i+1], NULL, 16);
+            dr_fprintf(STDERR, "Only instrumenting addresses before 0x%08x\n", instrument_end_address);
+            i++; 
+        }
+        else if (!strcmp(arg, "-module_name"))
+        {
+            instrument_module_name = argv[i+1];
+            dr_fprintf(STDERR, "Only printing code contained in module %s\n", instrument_module_name);
+            i++;
+        }
+        else if (!strcmp(arg, "-output_prefix"))
+        {
+            output_prefix = argv[i+1];
+            dr_fprintf(STDERR, "Output filename prefix is %s\n", output_prefix);
+            i++;
+        }
+        else
+        {
+            dr_fprintf(STDERR, "Incorrect argument.\n");
+            exit(0);
+        }
+    }
+}
+
 static void
 output_to_files(void)
 {
     char *filename = (char *) "test_output.coverage";
+
+    if (output_prefix != 0)
+    {
+        filename = dr_global_alloc(strlen(output_prefix) + strlen(".coverage") + 1);
+        memset(filename, 0x00, strlen(output_prefix) + strlen(".coverage") + 1);
+        strncpy(filename, output_prefix, strlen(output_prefix));
+        strncat(filename, ".coverage", strlen(".coverage"));
+    }
 
     FILE *fh = fopen(filename, "w+"); 
 
@@ -47,6 +110,8 @@ output_to_files(void)
     }
 
     fclose(fh);
+
+    fclose(mmap_fh);
 }
 
 static struct ThreadBBInfo * find_tinfo_entry(thread_id_t tid)
@@ -95,7 +160,7 @@ record_bb_edge(app_pc bb_addr)
         !found_bb_record(tinfo->last_bb, bb_addr)
        )
     {
-        dr_fprintf(STDOUT, "New observed block 0x%08x 0x%08x\n", tinfo->last_bb, bb_addr);
+        //dr_fprintf(STDOUT, "New observed block 0x%08x 0x%08x\n", tinfo->last_bb, bb_addr);
 
         struct BasicBlockRecord *new_record = dr_global_alloc(sizeof(struct BasicBlockRecord));
 
@@ -120,6 +185,16 @@ instrument_basic_block(void *drcontext, void *tag, instrlist_t *bb,
                        bool for_trace, bool translating
                       )
 {
+    if (instrument_start_address != 0 && instrument_end_address != 0)
+    {
+        app_pc bb_addr = dr_fragment_app_pc(tag);
+
+        if (bb_addr < instrument_start_address || bb_addr > instrument_end_address)
+        {
+            return DR_EMIT_DEFAULT;
+        }
+    }
+
     //Need to insert a call to our stub to manage our linked list of 
     //observed basic block transitions
     dr_insert_clean_call(drcontext, bb, instrlist_first(bb),
@@ -179,18 +254,41 @@ handle_thread_deinit(void *drcontext)
 static void
 handle_module_load(void *drcontext, const module_data_t *info, bool loaded)
 {
+    dr_fprintf(STDOUT, "Module loaded %s\n", info->full_path);
+
+    fprintf(mmap_fh, "%s,0x%08x,0x%08x\n", info->full_path, (uint32_t) info->entry_point, (uint32_t) info->end);
+
+    //Check to see if this is the module we were told to instrument code in
+    //
+    //If so then set start and end address to entire range off module image.
+    if (instrument_start_address == 0 && instrument_end_address == 0 && instrument_module_name != NULL)
+    {
+        char *fname_ptr = strrchr(info->full_path, '/');
+
+        if (!strncmp(fname_ptr+1, instrument_module_name, strlen(instrument_module_name)))
+        {
+            instrument_start_address = info->entry_point;
+            instrument_end_address = info->end;
+        }
+    }
+
+    //Probably missing some edge cases here due to module loading
+    //unloading but probably okay most of the time.
 }
 
 static void
 handle_module_unload(void *drcontext, const module_data_t *info)
 {
+    dr_fprintf(STDOUT, "Module unloaded %s\n", info->full_path);
 }
 
 DR_EXPORT void
 dr_client_main(client_id_t id, int argc, const char *argv[])
 {
     (void) thread_list;
-    dr_set_client_name("bbedge_coverage", "www.google.com");
+    dr_set_client_name("bbedge_coverage", "arc2205@columbia.edu");
+
+    parse_arguments(argc, argv);
 
     dr_register_exit_event(output_to_files);
 
@@ -213,6 +311,19 @@ dr_client_main(client_id_t id, int argc, const char *argv[])
 
     //Register thread deinitialization event for recording which thread
     dr_register_thread_exit_event(handle_thread_deinit);
+
+    //Open mmap_fh for writing dynamic module loading info to
+    char *mmap_filename = (char *) "test_output.mmap";
+
+    if (output_prefix != 0)
+    {
+        mmap_filename = dr_global_alloc(strlen(output_prefix) + strlen(".mmap") + 1);
+        memset(mmap_filename, 0x00, strlen(output_prefix) + strlen(".mmap") + 1);
+        strncpy(mmap_filename, output_prefix, strlen(output_prefix));
+        strncat(mmap_filename, ".mmap", strlen(".mmap"));
+    }
+
+    mmap_fh = fopen(mmap_filename, "wb+");
 
     dr_log(NULL, LOG_ALL, 1, "Client initialized\n");
 
